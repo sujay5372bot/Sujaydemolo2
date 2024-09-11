aef07574117258f683e069fa6a9f2abc96e98a43
sujay5372bot <sujay53721@gmail.com>
2024-09-11 15:37:29 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,4 +1,4 @@
-from info import DATABASE_URI
+from info import DATABASE_URI, SECONDDB_URI
 
 # Bot information
 SESSION = 'Media_search'
@@ -23,4 +23,4 @@ DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 #temp dict for storing the db uri which will be used for storing user, chat and file infos
-tempDict = {'indexDB': DATABASE_URI}
+tempDict = {'indexDB': SECONDDB_URI}
